5015166cc0ebf29053c229e0bc3d42d68c6b0993
tanumalik1 <143524820+tanumalik1@users.noreply.github.com>
2025-03-23 01:36:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+a = float(input("Enter the first number: "))
+b =float(input("Enter the second number: "))
+c = a+b
+d = (a-b)
+e = a*b
+f = a/b if b!=0 else "undefined"
+print("Addition" , c)
+print("Subtraction" , d)
+print("Multiplication" , e)
+print("Division" , f)
+
